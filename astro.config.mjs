fa360f39f54d0e5e142f54f69a5bc9816ac3b5ae
Diego <diesor53@gmail.com>
2024-08-26 00:30:14 -0500

    gh pages astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
-  base: " https://github.com/Diegosotomayor1/work-municipality-lima", // Reemplaza con el nombre de tu repositorio
+  site: "https://Diegosotomayor1.github.io",
+  base: "/work-municipality-lima",
 });
